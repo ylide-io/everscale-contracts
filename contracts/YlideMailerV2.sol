dd25b1b1d1e05ee6e8fa7f022b3a891e1704fd6d
Danila Simonov <ek@ek-mail.ru>
2022-07-06 21:30:10 +0200

    Mailer with registry test

--- contracts/YlideMailerV2.sol
+++ contracts/YlideMailerV2.sol
@@ -4,6 +4,9 @@ pragma AbiHeader pubkey;
 
 contract YlideMailerV2 {
 
+    mapping(address => uint256) public addressToPublicKey;
+    mapping(uint256 => address) public publicKeyToAddress;
+
     event MailPush(address sender, uint256 msgId, bytes key);
     event MailContent(address sender, uint256 msgId, uint16 parts, uint16 partIdx, bytes content);
 
@@ -13,29 +16,35 @@ contract YlideMailerV2 {
         tvm.accept();
     }
 
+    function attachPublicKey(uint256 publicKey) public {
+        addressToPublicKey[msg.sender] = publicKey;
+    }
+
+    function attachAddress(uint256 publicKey) public {
+        publicKeyToAddress[publicKey] = msg.sender;
+    }
+
     function buildHash(uint256 pubkey, uint32 uniqueId, uint32 time) public pure returns (uint256 _hash) {
-        // _bytes = new bytes(40);
-        // assembly { mstore(add(_bytes, 32), pubkey) }
-        // assembly { mstore(add(_bytes, 36), uniqueId) }
-        // assembly { mstore(add(_bytes, 40), time) }
-        // _hash = sha256(_bytes))
         _hash = sha256(bytes(bytes32(pubkey * uniqueId * time)));
     }
 
     // Virtual function for initializing bulk message sending
-    function getMsgId(uint32 uniqueId) public pure returns (uint256 msgId, uint32 initTime) {
+    function getMsgId(uint256 publicKey, uint32 uniqueId) public pure returns (uint256 msgId, uint32 initTime) {
         initTime = msg.createdAt;
-        msgId = buildHash(msg.pubkey(), uniqueId, initTime);
+        msgId = buildHash(publicKey, uniqueId, initTime);
     }
 
     // Send part of the long message
-    function sendMultipartMailPart(uint32 uniqueId, uint32 initTime, uint16 parts, uint16 partIdx, bytes content) public pure {
+    function sendMultipartMailPart(uint256 publicKey, uint32 uniqueId, uint32 initTime, uint16 parts, uint16 partIdx, bytes content) public {
         tvm.rawReserve(1 ton, 0);
 
         require(msg.createdAt >= initTime, 103);
         require(msg.createdAt - initTime >= 10 minutes, 104);
 
-        uint256 msgId = buildHash(msg.pubkey(), uniqueId, initTime);
+        attachPublicKey(publicKey);
+        attachAddress(publicKey);
+
+        uint256 msgId = buildHash(publicKey, uniqueId, initTime);
 
         // For indexation purposes
         address fakeContentAddr = address.makeAddrExtern(msgId, 256);
@@ -46,10 +55,10 @@ contract YlideMailerV2 {
     }
 
     // Add recipient keys to some message
-    function addRecipients(uint32 uniqueId, uint32 initTime, address[] recipients, bytes[] keys) public pure {
+    function addRecipients(uint256 publicKey, uint32 uniqueId, uint32 initTime, address[] recipients, bytes[] keys) public pure {
         tvm.rawReserve(1 ton, 0);
 
-        uint256 msgId = buildHash(msg.pubkey(), uniqueId, initTime);
+        uint256 msgId = buildHash(publicKey, uniqueId, initTime);
         
         for (uint i = 0; i < recipients.length; i++) {
             address recipientAddr = address.makeAddrExtern(recipients[i].value, 256);
@@ -59,10 +68,13 @@ contract YlideMailerV2 {
         msg.sender.transfer({ value: 0, flag: 128, bounce: false });
     }
 
-    function sendSmallMail(uint32 uniqueId, address recipient, bytes key, bytes content) public pure {
+    function sendSmallMail(uint256 publicKey, uint32 uniqueId, address recipient, bytes key, bytes content) public {
         tvm.rawReserve(1 ton, 0);
 
-        uint256 msgId = buildHash(msg.pubkey(), uniqueId, msg.createdAt);
+        attachPublicKey(publicKey);
+        attachAddress(publicKey);
+
+        uint256 msgId = buildHash(publicKey, uniqueId, msg.createdAt);
 
         // For indexation purposes
         address fakeContentAddr = address.makeAddrExtern(msgId, 256);
@@ -74,10 +86,13 @@ contract YlideMailerV2 {
         msg.sender.transfer({ value: 0, flag: 128, bounce: false });
     }
 
-    function sendBulkMail(uint32 uniqueId, address[] recipients, bytes[] keys, bytes content) public pure {
+    function sendBulkMail(uint256 publicKey, uint32 uniqueId, address[] recipients, bytes[] keys, bytes content) public {
         tvm.rawReserve(1 ton, 0);
 
-        uint256 msgId = buildHash(msg.pubkey(), uniqueId, msg.createdAt);
+        attachPublicKey(publicKey);
+        attachAddress(publicKey);
+
+        uint256 msgId = buildHash(publicKey, uniqueId, msg.createdAt);
 
         // For indexation purposes
         address fakeContentAddr = address.makeAddrExtern(msgId, 256);
